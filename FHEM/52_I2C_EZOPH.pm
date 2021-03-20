68f632302a39d58f014b4bcd99d791984d69ccb0
der-Lolo <der-lolo@pm.me>
2021-03-20 20:54:51 +0100

    ,mn

--- FHEM/52_I2C_EZOPH.pm
+++ FHEM/52_I2C_EZOPH.pm
@@ -159,8 +159,8 @@ sub I2C_EZOPH_Set($@) {
 
 	if ($cmd eq "readValues") {
 		$sleepmode = 0;
-		I2C_EZOPH_readpH($hash);
-		#I2C_EZOPH_Poll($hash);
+		#I2C_EZOPH_readpH($hash);
+		I2C_EZOPH_Poll($hash);
 	}
 	if ($cmd eq "TemperaturCompensation") {
 		I2C_SET_PHTEMPCOMP($hash,$val);
